c6fb346068506796f1307fcda89eb3e1c8a26c86
Raghunath D <raghu@caramail.com>
2018-08-17 08:04:04 +0530

    first commit

---
+++ dataUpload/mergeandunzip.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+cd /home/raghu/
+
+
+ls esparts
+if [ $? -neq 0 ]
+then
+	echo "esparts doesn't exist... exiting"
+    exit 1
+fi
+
+
+cd esparts
+
+cat esbackup.zip* > backup.zip
+tar -xvf backup.zip
+
+if [ $? -eq 0 ]
+then
+	echo "Successfully unzipped /esbackup folder."     
+    rm -rf ../esbackup/*    
+    mv esbackup/* ../esbackup/
+    
+    cd ..
+    rm -rf esparts
+    	
+    exit 0
+else
+	echo "Could not unzip /esbackup folder." 
+	exit 2
+fi
---
+++ dataUpload/zipandsplit.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+cd /home/raghu
+
+zip -r esbackup.zip esbackup
+
+mkdir -p esparts
+rm -rf esparts/*
+
+split --bytes=100MB esbackup.zip esparts/esbackup.zip_
+
+if [ $? -eq 0 ]; then
+	echo "Successfully completed split."
+    #add index name file to paths folder	
+    rm esbackup.zip
+	exit 0
+else
+	echo "Could not split." 
+	rm esbackup.zip
+	rm -rf esparts
+	exit 1
+fi
+
+
