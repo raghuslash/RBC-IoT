11fac11dc4bfa4b1414389d3a22eb16dfab34ecc
Raghunath <raghu@caramail.com>
2018-08-20 08:00:58 +0530

    RFO stateGen

--- state_extractor_stream.py
+++ state_extractor_stream.py
@@ -5,76 +5,69 @@ import numpy as np
 import yaml
 import os
 import sys
-class csv_data():
+class machine_state_generator():
     def __init__(self,filenames):
         with open(filenames,'r') as files:
             self.data_files=yaml.load(files)
-        self.loader_vibration_flag=False
-        self.sp_vibration_flag=False
-        self.sp_vaf_flag=False
-        self.pp1_vibration_flag=False
-        self.pp1_vaf_flag=False
-        self.pp2_vibration_flag=False
-        self.pp2_vaf_flag=False
-        self.reflow_vaf_flag=False
-    def loadData(self, machine):
+        self.loader_flag=False
+        self.sp_flag=False
+        self.pp1_flag=False
+        self.pp2_flag=False
+        self.reflow_flag=False
+    def _loadData(self, machine):
 
         pattern="%Y-%m-%dT%H:%M:%S.%f" # format of the timestamp
-        ################Loader's vibration data loading###################
-
-        print("loader vibration data started loading \n")
-        try  :
-            self.loader_vibration=pd.read_csv(self.data_files['file names']['loader']['vibration'],usecols=['data.timestamp','data.ax','data.ay','data.az'])
-            self.loader_vibration['data.timestamp']=self.loader_vibration['data.timestamp'].apply(lambda x: pd.to_datetime(x,format=pattern,box=False)+pd.Timedelta('05:30:00'))
-            self.loader_vibration.index=self.loader_vibration['data.timestamp']
-            self.loader_vibration.sort_index()
-            print("loader's vibration data finished loading \n")
-        except : 
-            print("Specified file doesn't exist,change in yaml file ") 
-            # sys.stderr.write('Ok quitting')
-            # sys.exit(1)
+        ################Loader's data loading###################
+        if(self.loader_flag):
+            print("loader vibration data started loading \n")
+            try  :
+                self.loader_vibration=pd.read_csv(self.data_files['file names']['loader']['vibration'],usecols=['data.timestamp','data.ax','data.ay','data.az'])
+                self.loader_vibration['data.timestamp']=self.loader_vibration['data.timestamp'].apply(lambda x: pd.to_datetime(x,format=pattern,box=False)+pd.Timedelta('05:30:00'))
+                self.loader_vibration.index=self.loader_vibration['data.timestamp']
+                self.loader_vibration.sort_index()
+                print("loader's vibration data finished loading \n")
+            except : 
+                print("Specified file doesn't exist, change path in yaml file ") 
+                sys.stderr.write('Ok quitting')
+                sys.exit(1)
         
         ###################################################################
 
         #######################pick and place1 data loading############################
+        elif (self.pp1_flag)
         print("pick and place data loading \n")
         print("pick and place vibration data loading \n")
         try :
             self.pp1_vibration=pd.read_csv(self.data_files['file names']['pick and place1']['vibration'],usecols=['data.timestamp','data.ax','data.ay','data.az'])
-
             self.pp1_vibration['data.timestamp']=self.pp1_vibration['data.timestamp'].apply(lambda x: pd.to_datetime(x,format=pattern,box=False)+pd.Timedelta('05:30:00'))
             self.pp1_vibration.index=self.pp1_vibration['data.timestamp']
             self.pp1_vibration.sort_index()
-            print("finish loading pickandplace1 vibration")
-        except : 
-            print("Specified file doesn't exist,change in yaml file ") 
-            # sys.stderr.write('Ok quitting')
-            # sys.exit(1)
-        #########################pick and place1 vaf loading############################
-        try :
             print("pick and place 1 vaf loading \n")
             self.pp1_vaf=pd.read_csv(self.data_files['file names']['pick and place1']['power'],usecols=['data.timestamp','data.A1','data.A2','data.A3'])
             self.pp1_vaf['data.timestamp']=self.pp1_vaf['data.timestamp'].apply(lambda x: pd.to_datetime(x,format=pattern,box=False)+pd.Timedelta('05:30:00'))
             self.pp1_vaf.index=self.pp1_vaf['data.timestamp']
             self.pp1_vaf.sort_index()
         except : 
-            print("Specified file doesn't exist,change in yaml file ") 
-            # sys.stderr.write('Ok quitting')
-            # sys.exit(1)
+            print("Specified file doesn't exist, change path in yaml file ") 
+            sys.stderr.write('Ok quitting')
+            sys.exit(1)
+
         #########################screen printer data loading############################
+        elif (self.sp_flag)
         try :
             print("screen printer data loading \n")
             print("screen printer power loading \n")
-            self.sp_vaf=pd.read_csv(self.data_files['file names']['screen printer']['power'],usecols=['data.timestamp','data.A1'])
+            self.sp_vaf=pd.read_csv(self.data_files['file names']['screen printer']['power'], usecols=['data.timestamp','data.A1','data.A2','data.A3'])
             self.sp_vaf['data.timestamp']=self.sp_vaf['data.timestamp'].apply(lambda x: pd.to_datetime(x,format=pattern,box=False)+pd.Timedelta('05:30:00'))
             self.sp_vaf.index=self.sp_vaf['data.timestamp']
             self.sp_vaf.sort_index()
         except :
-            print("Specified file doesn't exist,change in yaml file ") 
-            # sys.stderr.write('Ok quitting')
-            # sys.exit(1)
+            print("Specified file doesn't exist, change path in yaml file ") 
+            sys.stderr.write('Ok quitting')
+            sys.exit(1)
         ##################################################################################
         #########################reflow oven data loading############################
+        elif (self.rf_flag)        
         try :
             print("reflow oven data loading \n")
             print("reflow oven power loading \n")
@@ -83,57 +76,70 @@ class csv_data():
             self.rf_vaf.index=self.sp_vaf['data.timestamp']
             self.rf_vaf.sort_index()
         except :
-            print("Specified file doesn't exist,change in yaml file ") 
-            # sys.stderr.write('Ok quitting')
-            # sys.exit(1)
+            print("Specified file doesn't exist, change path in yaml file ") 
+            sys.stderr.write('Ok quitting')
+            sys.exit(1)
         ##################################################################################
 
-    def setMachine(self,machine,sensor):
-        #try :
-        self.loadData(machine)
-        mach=eval("self.%s_%s"%(machine,sensor))
-        self.machine=[]
-        self.machine = mach
-
-        if hasattr(self.machine,'attr_name'):
-            print("%s is not an attribute in the object csv_data"%(self.machine))
-        #except NameError :
-        #print("There's some problem!, pass machine and sensor as string '' !")
-        try:
-            #var=eval("%s_flag"%(self.machine))
-            temp='flag'
-            var=eval("self.%s_%s_flag"%(machine,sensor))
-            try :
-                if hasattr(var,'attr_name'):
-                    var=not(var)
-            except :
-                print("Error while setting the flag!!")
-        except NameError:
-            print("error!!")
+    def setMachine(self,machine):
+        eval("self.%s_flag"%(machine))=True
+        self._loadData(machine)
         return 
-    def setTimeFrame(self,start_time,stop_time):
-        self.start_time=start_time
-        self.stop_time=stop_time
-    
-    def getData(self,field):
-        return self.machine[self.start_time][self.stop_time][str("data."+field)].sort_index()
-    
-    # def analyse(self,start_time,stop_time):
-    #     pass
-    # def VibrationRollingVariance(self,start_time,stop_time,window):
-    #     self.varianceResult=self.machine[start_time:stop_time]['data.ax'].sort_index().rolling(window=window).var()
-    #     return self.varianceResult
-    
-    # def varianceThreshold(self,values,threshold):
-    #     variance_Threshold=pd.DataFrame((values > threshold).astype(np.int_))
-    #     variance_Threshold.reset_index(level=0, inplace=True)
-    #     return 
+
+    # def setTimeFrame(self,start_time,stop_time):
+
+    def genMachineStates(self):
+        if(self.rf_flag):
+            maxcur=62.0
+            roll_width = 275
+            #generate pointwise states
+            statep1 = pd.DataFrame(columns = ['timestamp', 'current', 'state'])
+            statep1['timestamp'] = self.rf_vaf['timestamp']
+            statep1['current'] = self.rf_vaf['data.A1']
+            statep1['state'] = np.where(self.rf_vaf['data.A1'] <= 1.5, 0, statep1['state'])
+            statep1['state'] = np.where(self.rf_vaf['data.A1'] >= .75 * maxcur1, 2, statep1['state'])
+            statep1['state'] = np.where((self.rf_vaf['data.A1'] <= .75 * maxcur1) & (dfp1['data.A1'] > 1.5), 1, statep1['state'])
+
+            #generate blockwise states
+            cur1 = pd.Series.to_frame(statep1.current.rolling(roll_width, center=True).mean())
+            st1['state'] = np.where(cur1['current'] <= 1.5, 0, st1['state'])
+            st1['state'] = np.where(cur1['current'] >= .42 * maxcur, 2, st1['state'])
+            st1['state'] = np.where((cur1['current'] <= .42 * maxcur) & (cur1['current'] > 1.5), 1, st1['state'])
+            pow1 = cur1
+            pow1['state']=st1['state']
+            pow1['power'] = 231 * cur1['current']
+
+            #PLot generated States
+            fig, ax1 = plt.subplots()
+            fig.set_size_inches(10,5)
+            ax1.plot(pow1['current'],color='orange')
+            ax1.xaxis.set_major_locator(mdates.DateFormatter('%H:%M'))
+            ax1.set_xlabel('Time')
+            ax1.set_ylabel('Current in Amps.', color='orange')
+            ax1.set_yticks(np.arange(0, 80, step=10))
+            ax1.tick_params('y', colors='orange')
+            ax2 = ax1.twinx()
+            ax2.plot(pow1['state'], 'r-', color='purple', alpha=0.5)
+            ax2.set_ylabel('State', color='purple')
+            ax2.tick_params('y', colors='purple')
+            ax2.set_yticks(np.arange(0, 3, step=1))
+            # ax2.set_yticks(np.arange(3), ('Off', 'Maintain', 'Heating'))
+            plt.title('Reflow Oven States')
+            # plt.show()
+            plt.savefig('Reflow_Oven_States.png')
+
+
+            #Return the dataFrame containing states
+            return(pow1, statep1)
+
+
+
+    def genNetworkLvlStates(self):
+
+
 
 
 if __name__=='__main__':
-    data=csv_data('data.yaml')
-    data.setMachine('rf','power')
-    # data.setTimeFrame('2018-03-14 10:30:00','2018-03-14 10:35:00')
-    data.getData('ax')
-    #data.setMachine('loader','vibration')
-    #loader_variance=data.VibrationRollingVariance('2018-03-14 10:30:00','2018-03-14 10:35:00',150)
+    data_rfo=machine_state_generator('data.yaml')
+    data_rfo.setMachine('reflow')
+    df_rf_states=data_rfo.genMachineStates()
